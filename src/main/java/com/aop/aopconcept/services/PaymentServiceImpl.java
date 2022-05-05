705958b4c17bcadf51b612af28f02a8bcfb0d1ca
ngactive99 <ngactive99@gmail.com>
2022-05-05 14:07:31 +0530

    first commit

---
+++ src/main/java/com/aop/aopconcept/services/PaymentServiceImpl.java
@@ -0,0 +1,18 @@
+package com.aop.aopconcept.services;
+
+public class PaymentServiceImpl implements PaymentService {
+
+	public void makePayment() {
+		// TODO Auto-generated method stub
+		
+		//payment 
+		System.out.println("Amount debited");
+		
+		System.out.println("Amount credited");
+		
+		
+		
+		
+	}
+
+}
