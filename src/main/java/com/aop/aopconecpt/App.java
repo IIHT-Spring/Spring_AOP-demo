b9b86b4eb86b50bac7f1a3387837f2a1d6a52da0
ngactive99 <ngactive99@gmail.com>
2022-05-05 14:23:42 +0530

    added aop dependency

--- src/main/java/com/aop/aopconecpt/App.java
+++ src/main/java/com/aop/aopconecpt/App.java
@@ -1,5 +1,10 @@
 package com.aop.aopconecpt;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.aop.aopconcept.services.PaymentService;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+//        System.out.println( "Hello World!" );
+    	
+    	ApplicationContext context = new ClassPathXmlApplicationContext("com/aop/aopconecpt/config.xml");
+    	
+    	PaymentService paymentObject = context.getBean("payment", PaymentService.class);
+    	paymentObject.makePayment(); // join point 
     }
 }
